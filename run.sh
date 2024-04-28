bcf03f9189bd0cfe76fd43022c12d6173f4177f9
fhdufhdu <fhdufhdu@gmail.com>
2024-04-28 21:57:58 +0900

    feat: 빌드 방식 변경

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@ if echo "$@" | grep -q "backend"; then
     cd backend
     ./gradlew clean && ./gradlew bootJar
     cd ..
-elif echo "$@" | grep -q "frontend"; then 
+fi
+if echo "$@" | grep -q "frontend"; then 
     cd frontend
     npm run build
     cd ..
