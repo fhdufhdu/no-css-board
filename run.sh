5a3f7f570c523752918e401c3ca0bdb204c8cdcf
lucas.ws <fhdufhdu@gmail.com>
2024-04-29 18:34:46 +0900

    feat: run.sh 파일 수정

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 if echo "$@" | grep -q "backend"; then 
     cd backend
+    git pull origin main
     ./gradlew clean && ./gradlew bootJar
     cd ..
 fi
 if echo "$@" | grep -q "frontend"; then 
     cd frontend
+    git pull origin main
+    npm install .
     npm run build
     cd ..
 fi
 
-docker compose up --build $@
+docker compose stop
+docker compose up -d --build $@
